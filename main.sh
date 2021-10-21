574d55ecf6f3042214b5476cde6528da3cb2cf68
faekz0r <priit228@gmail.com>
2021-10-22 01:42:40 +0300

    some fixes

--- main.sh
+++ main.sh
@@ -25,24 +25,27 @@ keep_charging_hours_only
 echo "resorted_prices.csv:"
 cat resorted_prices.csv
 
+if [ -z "$charge_for_hours" ]; then
 
-wake_tesla
-sleep 3
+	wake_tesla
+	sleep 3
 
-check_charge_state
-sleep 3
 
-if [ $(( $battery_level + $no_charge_buffer )) -ge "$charge_limit" ]; then
-	echo "No need to charge, since charge limit is at: $charge_limit%"
-	echo "and battery level is at: $battery_level%"
-	exit
+	check_charge_state
+	sleep 3
+
+	if [ $(( $battery_level + $no_charge_buffer )) -ge "$charge_limit" ]; then
+		echo "No need to charge, since charge limit is at: $charge_limit%"
+		echo "and battery level is at: $battery_level%"
+		exit
+	fi
+
 fi
-
 
 echo "seconds_to_limit:" $seconds_to_limit
 echo "charge_for_hours:" $charge_for_hours
 
-for i in $(seq 1 $charge_for_hours);
+for i in $(seq 1 $charge_for_hours)
 do
 	cheap_hour_start_csv=$(sed -n $i{p} resorted_prices.csv)
 	cheap_hour_start_stripped=$( echo $cheap_hour_start_csv | awk -F "," '{ print $1 }' )
@@ -61,11 +64,8 @@ do
 		sleep $(( $cheap_hour_start_stripped - $(date +%s) ))
 	else
 		sleep $(( $(date +%s) - $cheap_hour_start_stripped ))
+	fi
 	
 	charge_stop
 
 done
-
-sleep 10
-
-/bin/bash main.sh
